a1f711a0b598fcd8b16b43e45da66ced873f7aa1
Joshua <link7844@gmail.com>
2022-06-08 18:33:08 -0700

    improve spacing in files

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,25 +6,25 @@ export default class About extends Component {
   render() {
     return (
       <div>
-         <div>
-        <div class="split left">
-          <div className="centered">
-            <img 
-              className="profile_image"
-                src={picture}
-              alt="Profile Pic"
-              ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Joshua Holdridge</div>
-            <div className="brief_description">
-              My name is Joshua Holdridge, I usually go by Josh. I am a Computer Science student at the University of Nevada, Las Vegas. I love to play fighting games and learn about programming.
+          <div>
+            <div class="split left">
+              <div className="centered">
+                <img 
+                  className="profile_image"
+                  src={picture}
+                  alt="Profile Pic"
+                ></img>
+              </div>
+            </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Joshua Holdridge</div>
+              <div className="brief_description">
+                My name is Joshua Holdridge, I usually go by Josh. I am a Computer Science student at the University of Nevada, Las Vegas. I love to play fighting games and learn about programming.
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     )
   }
